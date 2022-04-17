0fe1d947385488f688a5c99adff92e8dcb22c676
E. Ewusi-Essel <45021733+ewusiessel@users.noreply.github.com>
2022-04-17 20:00:27 +0100

    Update ProductCard.js

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -12,7 +12,7 @@ const ProductCard = () => {
             margin: "20px 20px"
           }}
         >
-          <Card.Img variant="top" src="https://picsum.photos/id/1001/500/500" />
+          <Card.Img variant="top" src="https://picsum.photos/id/102/500/500" />
           <Card.Body>
             <Card.Title>Kayaking</Card.Title>
             <Card.Text>
@@ -33,8 +33,9 @@ const ProductCard = () => {
           <Card.Body>
             <Card.Title>Tree Climbing</Card.Title>
             <Card.Text>
-              Whether you want to learn new skills, improve your knowledge, try
-              something new or enjoy the outdoors, everyone deserves adventure.
+              We deliver a range of courses, taster sessions or adventures for
+              trail running, skyrunning, rock climbing, mountain biking,
+              scrambling and more.
             </Card.Text>
             <Button variant="danger">Sign Up</Button>
           </Card.Body>
@@ -49,9 +50,9 @@ const ProductCard = () => {
           <Card.Body>
             <Card.Title>Biking</Card.Title>
             <Card.Text>
-              Climbing has seen a huge increase in numbers over the last few
-              years. With the majority of people learning on artificial climbing
-              walls. Give it shot!
+              We deliver a range of courses, taster sessions or adventures for
+              trail running, skyrunning, rock climbing, mountain biking,
+              scrambling and more.
             </Card.Text>
             <Button variant="warning">Sign Up</Button>
           </Card.Body>
